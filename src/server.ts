712781d1615b9bb3f4e3ccca06395bbf3f509648
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-07-28 13:36:22 +0600

    feat: add environment variable configuration and update server to use env variables
    - Added dotenv configuration to load environment variables from a .env file.
    - Created a new env.ts file to manage and validate required environment variables.
    - Updated server.ts to use PORT and NODE_ENV from environment variables instead of hardcoded values.
    - Added eslint script to package.json for linting the source code.
    - Updated devDependencies in package.json to include eslint and typescript-eslint.

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
-import { Server } from "http";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Server } from "http"; // ✅ type-only import
 
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 
 const startServer = async () => {
   try {
+
+    console.log(envVars.NODE_ENV);
+    
     await mongoose.connect("mongodb+srv://tour-management:UL9NNGTjgnnV0Znx@cluster0.ta90r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     console.log("Connected to DB!!");
 
-    server = app.listen(5000, () => {
-      console.log("Server running on http://localhost:5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running on port ${envVars.PORT} in ${envVars.NODE_ENV} mode`);
     });
   } catch (error) {
     console.error("Failed to connect to DB:", error);
