be216e2b0f238032919c22805f26a4aafd1ac6f4
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-12-02 14:14:40 +0600

    27-10 5.56 minutes

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import AppError from "../../errorHelpers/AppError";
-import { IAuthProvider, IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import User from "./user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 // Create a new user
 const createUser = async (payload: Partial<IUser>): Promise<IUser> => {
   const { email, password, ...rest } = payload;
@@ -14,13 +16,12 @@ const createUser = async (payload: Partial<IUser>): Promise<IUser> => {
     throw new AppError("User already Exists", httpStatus.BAD_REQUEST);
   }
 
-  const hashedPassword = await bcryptjs.hash(password as string,Number(envVars.BCRYPT_SALT_ROUND));
+  const hashedPassword = await bcryptjs.hash(
+    password as string,
+    Number(envVars.BCRYPT_SALT_ROUND)
+  );
 
-// const isPasswordMatch = await bcryptjs.compare(password as string, hashedPassword);
-
-  
-
-
+  // const isPasswordMatch = await bcryptjs.compare(password as string, hashedPassword);
 
   const authProvider: IAuthProvider = {
     provider: "credentials",
@@ -35,6 +36,38 @@ const createUser = async (payload: Partial<IUser>): Promise<IUser> => {
   });
 
   return user;
+};
+
+const updateUser = async (
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload
+) => {
+  /**
+   * email- cannot be updated
+   * name,phone,passwrd,addreess
+   * password- can be updated
+   * ONLY ADMIN, SUPER ADMIN CAN UPDATE
+   * promoting to super admin - superadmin
+   */
+
+  if (payload.role !== decodedToken.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError("You are not authorized", httpStatus.FORBIDDEN);
+    }
+
+    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+      throw new AppError ("You are not authorized", httpStatus.FORBIDDEN);
+    }
+  }
+  if(payload.isActive || payload.isDeleted || payload.isVerified){
+  if(decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE){
+    throw new AppError ("You are not authorized", httpStatus.FORBIDDEN);
+  }
+  }
+
+
+//  start from here***********************
 };
 
 // Get all users with optional total count
